17af61b4b79a61d78fa449009f5825fd194a52e9
ANDRE FIGUEIREDO ALMEIDA <8200289@estg.ipp.pt>
2022-05-31 12:10:44 +0100

    aviso de nome nos terminais

--- startAPP.bat
+++ startAPP.bat
@@ -1,4 +1,4 @@
 echo on
-START cmd /c "cd /d %CD%\Rest-api-back-end\. && npm i && npm start"
-START cmd /c "cd /d %CD%\Angular-front-end\. && npm i && npm start"
+START cmd /c "ECHO -------------REST API------------- && cd /d %CD%\Rest-api-back-end\. && npm i && npm start"
+START cmd /c "ECHO -------------BACK END------------- && cd /d %CD%\Angular-front-end\. && npm i && npm start"
 
