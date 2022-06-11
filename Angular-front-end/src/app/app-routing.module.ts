756c50014dc7c02e86d3832abdb847aab56777b0
8180148 <8180148@estg.ipp.pt>
2022-06-11 15:18:25 +0100

    routs q faltam

--- Angular-front-end/src/app/app-routing.module.ts
+++ Angular-front-end/src/app/app-routing.module.ts
@@ -69,6 +69,14 @@ const routes: Routes = [
   { path: 'transaction-add', component: TransactionAddComponent, canActivate: [AuthGuard,RoleGuard] },
   { path: 'transaction-edit/:id', component: TransactionEditComponent , canActivate: [AuthGuard,RoleGuard]},
 
+  { path: 'notifications', component: NotificationsListingComponent, canActivate: [AuthGuard,RoleGuard]},
+  { path: 'notification-details/:id', component: NotificationDetailsComponent, canActivate: [AuthGuard,RoleGuard]},
+  { path: 'notification-add', component: NotificationAddComponent, canActivate: [AuthGuard,RoleGuard] },
+  { path: 'notification-edit/:id', component: NotificationEditComponent , canActivate: [AuthGuard,RoleGuard]},
+
+  { path: 'cliente-page', component: ClientePageComponent },
+  { path: 'employee-page', component: EmployeePageComponent },
+
   { path: 'proposals', component: ProposalsListingComponent, /* canActivate: [AuthGuard] */},
   { path: 'proposal-details/:id', component: ProposalDetailsComponent, /*canActivate: [AuthGuard] */},
   { path: 'proposal-add', component: ProposalAddComponent , /* canActivate: [AuthGuard]*/},
